376462e29570ddbd844648c7dfb1fc57a7889894
Nkaleth <nseguralu@gmail.com>
2023-04-12 23:30:48 -0500

    ADD rental, book and classroom classes according requeriments

---
+++ rental.rb
@@ -0,0 +1,14 @@
+class Rental
+  attr_reader :book, :person
+  attr_accessor :date
+
+  def initialize(person, book, date)
+    @date = date
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
+  end
+end
